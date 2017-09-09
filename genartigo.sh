3e7646e447d167f64c45ab3a295c93c5c5ed70dd
Cleibson Almeida <nosbielcs@bol.com.br>
2017-09-09 20:17:02 -0300

    melhoria no gen

--- genartigo.sh
+++ genartigo.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#genartigo.sh - Script que gera um template de artigo científico em Latex
+#
+#Desenvolvido por Cleibson Almeida <nosbielcs@bol.com.br>
+#
+#Dependências
+##Precisa ter o pacote texlive-full instalado em sua distribuição linux
+
 #instalação do texlive
 #>sudo apt-get install texlive-full
 
@@ -14,14 +21,15 @@ cat resources/format_default.alm > template/elsearticle.cls
 sleep 5
 echo "conteúdo base foi criado no diretório /template"
 
-#compila latex
+#compila latex - depende de ter o texlive-full instalado
 cd template
 pdflatex main.tex
 cd ..
 sleep 5
 echo "arquivo main.tex foi compilado com sucesso!"
 
-#mensagem ao usuario
+#mensagem de sucesso ao usuario
 sleep 5
 echo "template criada com sucesso!"
 echo "imagens estão no diretório /images"
+
